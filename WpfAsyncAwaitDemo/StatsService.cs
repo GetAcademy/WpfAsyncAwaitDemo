ed07a1ecdd2a7c298c83176d3f7579be0a9e3c46
GET Academy <terje@kolderup.net>
2021-02-05 11:33:43 +0100

    x

---
+++ WpfAsyncAwaitDemo/StatsService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfAsyncAwaitDemo
+{
+    class StatsService
+    {
+        private IOrderRepository _orderRepository;
+
+        public StatsService(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        int GetTotalOrdersAmount()
+        {
+            var orders = _orderRepository.GetAll();
+            var sum = 0;
+            foreach (var order in orders)
+            {
+                sum += order.Amount;
+            }
+
+            return sum;
+        }
+    }
+
+    interface IDb
+    {
+        IEnumerable<T> Query<T>(string sql);
+    }
+
+    class Order
+    {
+        public int Amount { get; }
+    }
+
+    interface IOrderRepository
+    {
+        IEnumerable<Order> GetAll();
+    }
+    //class OrderRepository : IOrderRepository
+    //{
+    //    IEnumerable<Order>
+    //}
+}
