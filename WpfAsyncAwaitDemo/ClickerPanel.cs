a39fe220051b62c002be57fd361a97adabd79333
GET Academy <terje@kolderup.net>
2021-02-05 09:56:11 +0100

    Initial commit

---
+++ WpfAsyncAwaitDemo/ClickerPanel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp7
+{
+    class ClickerPanel : StackPanel
+    {
+        private Label _label;
+        private int _count;
+        public ClickerPanel()
+        {
+            var button = new Button() { Content = "Klikk her" };
+            _label = new Label();
+            button.Click += ButtonClick;
+            Children.Add(button);
+            Children.Add(_label);
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            _count++;
+            _label.Content = _count;
+        }
+    }
+}
---
+++ WpfAsyncAwaitDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp7
+{
+    class Program
+    {
+        private static Label _label;
+        private static int _count;
+
+        [STAThread]
+        public static void Main()
+        {
+            var application = new Application();
+            var window = new Window();
+
+            var panel = new StackPanel();
+            panel.Children.Add(new ClickerPanel());
+            panel.Children.Add(new ClickerPanel());
+            panel.Children.Add(new ClickerPanel());
+            panel.Children.Add(new ClickerPanel());
+            window.Content = panel;
+
+            application.Run(window);
+        }
+
+
+    }
+}
